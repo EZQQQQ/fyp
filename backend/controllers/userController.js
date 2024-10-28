0bbeae14cd06d4f315ef010e52579bcb51d98161
Ezekiel Loh <ezekiel.loh5265@gmail.com>
2024-10-29 04:34:01 +0800

    Replace bcrypt with bcryptjs to fix deployment issues

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 // /backend/controllers/userController.js
 
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // Register a new user
