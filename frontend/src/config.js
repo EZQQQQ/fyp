dd14cb310cd986b204b84732019247a5a3392087
Ezekiel Loh <ezekiel.loh5265@gmail.com>
2024-12-01 08:22:03 +0800

    fixed profilePicture display

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
 // /src/config.js
 
-const BACKEND_URL = "http://localhost:5001";
+const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:5001";
 
 export default {
   BACKEND_URL,
